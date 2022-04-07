d545ea1703c7972213a4ab4e22994421a20264ec
heano kim <heanokim@yahoo.com>
2022-04-07 12:53:47 +0700

    Removed unnecessary code

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-// don't know if I need this
+// // don't know if I need this
 
-const userPassword = document.getElementById("password");
-const confirmPassword = document.getElementById("confirm-password");
+// const userPassword = document.getElementById("password");
+// const confirmPassword = document.getElementById("confirm-password");
 
-console.log(userPassword);
+// console.log(userPassword);
 
-function validatePassword() {
-    if (userPassword.value != confirmPassword.value) {
-        confirmPassword.setCustomValidity("Passwords don't match.")
-    } else {
-        confirmPassword.setCustomValidity("")
-    }
-}
+// function validatePassword() {
+//     if (userPassword.value != confirmPassword.value) {
+//         confirmPassword.setCustomValidity("Passwords don't match.")
+//     } else {
+//         confirmPassword.setCustomValidity("")
+//     }
+// }
 
-// userPassword.onchange = validatePassword();
-// confirmPassword.onkeyup = validatePassword();
+// // userPassword.onchange = validatePassword();
+// // confirmPassword.onkeyup = validatePassword();
 
-// show error message if email doesn't follow expected structure
+// // show error message if email doesn't follow expected structure
 
-const userEmail = document.getElementById("email");
+// const userEmail = document.getElementById("email");
 
-function ValidateEmail(userEmail) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail))
-  {
-    return (true)
-  }
-    alert("You have entered an invalid email address!")
-    return (false)
-}
+// function ValidateEmail(userEmail) 
+// {
+//  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail))
+//   {
+//     return (true)
+//   }
+//     alert("You have entered an invalid email address!")
+//     return (false)
+// }
 
-ValidateEmail();
+// ValidateEmail();
 
-// when user submits the form, run the password validation and show error message if the passwords don't match
+// // when user submits the form, run the password validation and show error message if the passwords don't match
